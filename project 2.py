e8b4e0ec338dc11f29bdea13218a4c0288bf60d9
Dinkarkumar2007 <dinkarkumar454@gmail.com>
2026-02-09 00:46:31 +0530

    Add files via upload

---
+++ project 2.py
@@ -0,0 +1,18 @@
+from turtle import *
+
+speed(0)
+bgcolor("black")
+colors = ['orange', 'white']
+hideturtle()
+
+for i in range(122):
+    color(colors[i % 2])
+    goto(0, 0)
+    forward(130)
+    left(3)
+    circle(40)
+    forward(130)
+    right(100)
+
+done()
+
